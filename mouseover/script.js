8f6635dd7a8bcbcb3d40a8894d6e6386f11cc2f4
Abhay Kant <abhaykantdwivedi731@gmail.com>
2026-09-15 23:56:41 +0530

    Mouseover Using Javascrip

---
+++ mouseover/script.js
@@ -0,0 +1,12 @@
+let over = document.querySelector("#over");
+
+over.addEventListener("mouseover",function(){
+    over.style.backgroundColor="yellow";
+
+});
+
+
+over.addEventListener("mouseout",function(){
+    over.style.backgroundColor="red";
+
+});
